a1c0798708624563d6719d627fa6bc18b5451a32
Paweł Jadach <pawel.jadach.96@gmail.com>
2019-11-24 14:48:40 +0100

    Add PostList.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import './App.css';
 //import SignIn from './components/SignIn.js';
 import NavBar from './components/NavBar';
-import Card from './components/Card';
+import PostList from './components/PostList';
 
 function App() {
   return (
     <>
       <NavBar />
       {/* <SignIn /> */}
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <PostList />
     </>
   );
 }
---
+++ src/components/PostList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Card from './Card';
+
+class PostList extends Component {
+  state = {};
+
+  render() {
+    return <Card />;
+  }
+}
+
+export default PostList;
