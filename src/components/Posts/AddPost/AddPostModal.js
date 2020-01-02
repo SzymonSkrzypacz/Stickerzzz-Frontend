7a84ca7930f92dd294abaf4e56b3538a775a0181
Paweł Jadach <pawel.jadach.96@gmail.com>
2020-01-02 17:09:37 +0100

    edit addpostmodal

--- src/components/Posts/AddPost/AddPostModal.js
+++ src/components/Posts/AddPost/AddPostModal.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-// import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
 
 import { withStyles } from '@material-ui/core/styles';
 
-// import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -12,29 +10,8 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Avatar from '@material-ui/core/Avatar';
 import { green } from '@material-ui/core/colors';
-import Geocode from "react-geocode";
-// import ReactLeafletSearch from "react-leaflet-search";
-import {
-  geocodeByAddress,
-  getLatLng }
- from 'react-places-autocomplete';
-// Geocode.setApiKey("AIzaSyCYCwv6-2J0fM3DQxVQJ_jM8rekqy9Mm8w");
 
-// Geocode.enableDebug();
-// const SearchComponent = props => (
-//     <ReactLeafletSearch
-//       position="topleft"
-//       inputPlaceholder="The default text in the search bar"
-//       search={[]} // Setting this to [lat, lng] gives initial search input to the component and map flies to that coordinates, its like search from props not from user
-//       zoom={7} // Default value is 10
-//       showMarker={true}
-//       showPopup={false}
-//       openSearchOnLoad={false} // By default there's a search icon which opens the input when clicked. Setting this to true opens the search by default.
-//       closeResultsOnClick={false} // By default, the search results remain when you click on one, and the map flies to the location of the result. But you might want to save space on your map by closing the results when one is clicked. The results are shown again (without another search) when focus is returned to the search input.
-//       providerOptions={{searchBounds: []}} // The BingMap and OpenStreetMap providers both accept bounding coordinates in [se,nw] format. Note that in the case of OpenStreetMap, this only weights the results and doesn't exclude things out of bounds.
-//       customProvider={undefined | {search: (searchString)=> {}}} // see examples to usage details until docs are ready
-//   />
-// )
+
 
 
 const styles = theme => ({
